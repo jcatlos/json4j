cca234a78c9da778c06bbc98db6c56957b540d23
Jakub Catlos <jakub.catlos@gmail.com>
2022-05-16 19:40:36 +0200

    Finished outline of the logic

--- src/main/java/json4j/JSONParser.java
+++ src/main/java/json4j/JSONParser.java
@@ -7,37 +7,66 @@ import org.apache.commons.lang.StringEscapeUtils;
 
 public class JSONParser {
 
+    int index;
+    char current_char;
+    String source;
+    Stack<JSONElementBuilder> scope;
+
     // CHARACTER CONSTANTS
-    char COLON      = ':';
-    char COMMA      = ',';
-    char QUOTE      = '"';
-    char ESCAPE     = '\\';
+    static char COLON      = ':';
+    static char COMMA      = ',';
+    static char QUOTE      = '"';
 
-    char START_OBJECT   = '{';
-    char END_OBJECT     = '}';
-    char START_ARRAY    = '[';
-    char END_ARRAY      = ']';
+    static char START_OBJECT   = '{';
+    static char END_OBJECT     = '}';
+    static char START_ARRAY    = '[';
+    static char END_ARRAY      = ']';
 
-    char[] WHITESPACE = {' ', '\t', '\n', '\r'};
+    static char[] WHITESPACE = {' ', '\t', '\n', '\r'};
 
-    private boolean is_whitespace(char c){
+
+    public JSONParser(String source_string){
+
+        // Handle all unicode character sequences in the source
+        source = StringEscapeUtils.unescapeJava(source_string);
+
+        scope = new Stack<>();
+
+        // Initialize reading index and current_char at the beginning
+        index = 0;
+        if(source.length() > 0){
+            current_char = source.charAt(0);
+        }
+    }
+
+    private static boolean is_whitespace(char c){
         for(char ws: WHITESPACE){
             if(ws == c) return true;
         }
         return false;
     }
 
-    public static ArrayList<JSONValue> parseString(String source_string){
+    private String get_next_token(){
+        // TODo
+        // Token = Zatvorka, dvojbodka, ciarka, uvodzovka, string
+        return "";
+    }
+
+    private void evaluate_token(String token){
+        //TODO
+    }
+
+    // Pridat streamove citanie?
+    public ArrayList<JSONValue> parseString(String source_string) throws JSONMalformedSourceException {
 
         // The list of JSON values from the source
         ArrayList<JSONValue>  values = new ArrayList<>();
+        JSONValue read_value;
 
-        // Handle all unicode character sequences in the source
-        String source = StringEscapeUtils.unescapeJava(source_string);
-
-        // The top of the stack represents the current parsed JSON object or list
-        Stack<JSONValue> scope = new Stack<>();
-
+        while(index < source.length()){
+            String token = this.get_next_token();
+            evaluate_token(token);
+        }
 
         return values;
     }
