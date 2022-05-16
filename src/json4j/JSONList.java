caf6fb47663b2855ca17a9093437d6d806f202c5
Jakub Catlos <jakub.catlos@gmail.com>
2022-05-16 15:17:09 +0200

    Implemented JSON list

--- src/json4j/JSONList.java
+++ src/json4j/JSONList.java
@@ -1,4 +1,21 @@
 package json4j;
 
+import java.util.LinkedList;
+
 public class JSONList {
+    private LinkedList<JSONValue> values;
+    private int length;
+
+    public JSONValue get(int index){
+        return values.get(index);
+    }
+
+    public void add(JSONValue value){
+        values.add(value);
+    }
+
+    public JSONList(){
+        this.values = new LinkedList<JSONValue>();
+        this.length = 0;
+    }
 }
