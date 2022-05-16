00367cf17f9d67f638624fdda8c17126731964c1
Jakub Catlos <jakub.catlos@gmail.com>
2022-05-16 19:37:48 +0200

    Created builder classes for large types

---
+++ src/main/java/json4j/JSONListBuilder.java
@@ -0,0 +1,7 @@
+package json4j;
+
+import java.util.ArrayList;
+
+public class JSONListBuilder extends JSONElementBuilder {
+    ArrayList<JSONValue> elements;
+}
---
+++ src/main/java/json4j/JSONObjectBuilder.java
@@ -0,0 +1,7 @@
+package json4j;
+
+import java.util.HashMap;
+
+public class JSONObjectBuilder extends JSONElementBuilder {
+    HashMap<String, JSONValue> elements;
+}
