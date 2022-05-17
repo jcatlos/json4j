d18170be0da65f90f24e97698c0f956fd562b72d
Jakub Catlos <jakub.catlos@gmail.com>
2022-05-17 12:55:15 +0200

    Implemented token class

---
+++ src/main/java/json4j/JSONToken.java
@@ -0,0 +1,92 @@
+package json4j;
+
+enum TOKEN_TYPE {
+    START_OBJECT,
+    END_OBJECT,
+    START_ARRAY,
+    END_ARRAY,
+    ELEMENT_DELIMITER,
+    KEY_VALUE_DELIMITER,
+    STRING_LITERAL,
+    NUMBER,
+    BOOLEAN,
+    NULL,
+    INVALID_TOKEN
+}
+
+public class JSONToken {
+
+    // CHARACTER CONSTANTS
+    final static String KEY_VALUE_DELIMITER = ":";
+    final static String ELEMENT_DELIMITER   = ",";
+
+    final static String START_OBJECT  = "{";
+    final static String END_OBJECT    = "}";
+    final static String START_ARRAY   = "[";
+    final static String END_ARRAY     = "]";
+
+    public final TOKEN_TYPE type;
+    public final Object value;
+
+    public JSONToken(String token_string){
+        switch (token_string){
+            case START_OBJECT -> {
+                this.type  = TOKEN_TYPE.START_OBJECT;
+                this.value = START_OBJECT;
+            }
+            case END_OBJECT -> {
+                this.type  = TOKEN_TYPE.END_OBJECT;
+                this.value = END_OBJECT;
+            }
+            case START_ARRAY -> {
+                this.type  = TOKEN_TYPE.START_ARRAY;
+                this.value = START_ARRAY;
+            }
+            case END_ARRAY -> {
+                this.type  = TOKEN_TYPE.END_ARRAY;
+                this.value = END_ARRAY;
+            }
+            case ELEMENT_DELIMITER -> {
+                this.type  = TOKEN_TYPE.ELEMENT_DELIMITER;
+                this.value = ELEMENT_DELIMITER;
+            }
+            case KEY_VALUE_DELIMITER -> {
+                this.type  = TOKEN_TYPE.KEY_VALUE_DELIMITER;
+                this.value = KEY_VALUE_DELIMITER;
+            }
+            case "true", "false" -> {
+                this.type  = TOKEN_TYPE.BOOLEAN;
+                this.value = token_string.equals("true");
+            }
+            case "null" -> {
+                this.type  = TOKEN_TYPE.NULL;
+                this.value = null;
+            }
+            default -> {
+                // Check if the value is enclosed in quotes
+                if(token_string.startsWith("\"") && token_string.endsWith("\"")){
+                    this.type = TOKEN_TYPE.STRING_LITERAL;
+                    this.value = token_string.substring(1, token_string.length()-1);
+                    return;
+                }
+
+                // Or if it is a number
+                TOKEN_TYPE temp_type;
+                Object temp_value;
+                try{
+                    temp_type  = TOKEN_TYPE.NUMBER;
+                    temp_value = Integer.parseInt(token_string);
+                }
+                catch (NumberFormatException e){
+                    // Otherwise token is invalid
+                    temp_type  = TOKEN_TYPE.INVALID_TOKEN;
+                    temp_value = null;
+                }
+
+                this.type  = temp_type;
+                this.value = temp_value;
+
+            }
+        }
+    }
+}
