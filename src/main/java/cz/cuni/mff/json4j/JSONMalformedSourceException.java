36ec55081b81084b71be74ccd8fd687da840aab0
Jakub Catlos <jakub.catlos@gmail.com>
2022-05-17 20:36:07 +0200

    Added error message

--- src/main/java/cz/cuni/mff/json4j/JSONMalformedSourceException.java
+++ src/main/java/cz/cuni/mff/json4j/JSONMalformedSourceException.java
@@ -1,4 +1,7 @@
 package cz.cuni.mff.json4j;
 
 public class JSONMalformedSourceException extends Exception{
+    public JSONMalformedSourceException(String message){
+        super(message);
+    }
 }
