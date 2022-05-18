9cdcb9fa6f695ace10c35140b1d560e680f0e3c5
Jakub Catlos <jakub.catlos@gmail.com>
2022-05-18 14:49:05 +0200

    Added toString() implementation

--- src/main/java/cz/cuni/mff/json4j/JSONObject.java
+++ src/main/java/cz/cuni/mff/json4j/JSONObject.java
@@ -1,10 +1,14 @@
 package cz.cuni.mff.json4j;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class JSONObject {
     private HashMap<String, JSONValue> values;
 
+    // Pridat aj set funkciu
     public void add(String key, JSONValue value){
         values.put(key, value);
     }
@@ -20,5 +24,24 @@ public class JSONObject {
     public JSONObject(){
         this.values = new HashMap<>();
 
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder out = new StringBuilder();
+        out.append('{');
+        for (Entry<String, JSONValue> stringJSONValueEntry : this.values.entrySet()) {
+            var pair = (Entry) stringJSONValueEntry;
+            out.append("\"");
+            out.append(pair.getKey());
+            out.append("\"");
+            out.append(':');
+            out.append(pair.getValue().toString());
+            out.append(',');
+        }
+        out.deleteCharAt(out.length()-1);
+        out.append('}');
+
+        return out.toString();
     }
 }
