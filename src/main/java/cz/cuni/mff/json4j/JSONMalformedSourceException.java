8c5709a96d0b24737e2079ff7fec4d799a64cad7
Jakub Catlos <jakub.catlos@gmail.com>
2022-05-19 17:17:44 +0200

    Javadocs for exceptions

--- src/main/java/cz/cuni/mff/json4j/JSONMalformedSourceException.java
+++ src/main/java/cz/cuni/mff/json4j/JSONMalformedSourceException.java
@@ -1,5 +1,14 @@
 package cz.cuni.mff.json4j;
 
+
+/**
+ * An exception when the parsed JSON is breaking any rules.
+ * The rules are specified in RFC-8252.
+ * Notable errors are: missing colons and commas, unfinished objects and arrays,
+ * non-string keys of objects or invalid literals.
+ * A message is provided to describe the found error.
+ * Upon throwing this exception, the parsing of the string is stopped.
+ */
 public class JSONMalformedSourceException extends Exception{
     public JSONMalformedSourceException(String message){
         super(message);
--- src/main/java/cz/cuni/mff/json4j/JSONUnfinishedStringAtEOF.java
+++ src/main/java/cz/cuni/mff/json4j/JSONUnfinishedStringAtEOF.java
@@ -1,4 +1,9 @@
 package cz.cuni.mff.json4j;
 
+/**
+ * An exception when the JSON ends with an unfinished String (missing the second quotechar).
+ * A JSON ending with an unfinished String is either malformed or corrupted.
+ * Note that this exception happens even if ANY String in the file is unfinished.
+ */
 public class JSONUnfinishedStringAtEOF extends Exception {
 }
